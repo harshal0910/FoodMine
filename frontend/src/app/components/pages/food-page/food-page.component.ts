3cf855f91a29a0a5b33ce23e532bb6ef760ab309
7147_harshal <harshalnagtilak2809@gmail.com>
2024-01-23 08:33:05 +0530

    Adding items in cart started.

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Food } from '../../../Shared/Models/Food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
 import { StarRatingComponent } from '../../partials/star-rating/star-rating.component';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../../services/cart.service';
 @Component({
   selector: 'app-food-page',
   standalone: true,
@@ -11,9 +12,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: './food-page.component.html',
   styleUrl: './food-page.component.css'
 })
-export class FoodPageComponent {
+export class FoodPageComponent implements OnInit {
   food!:Food
-  constructor(activatedRoute:ActivatedRoute,foodService:FoodService){
+  constructor(activatedRoute:ActivatedRoute,foodService:FoodService,private cartService:CartService,private router:Router){
     activatedRoute.params.subscribe((params)=>{
       if(params.id)
       {
@@ -21,4 +22,14 @@ export class FoodPageComponent {
       }
     })
   }
+
+  ngOnInit(): void {
+    
+  }
+
+  addTOCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
+  
 }
